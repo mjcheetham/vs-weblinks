90cf738cff2d6bd2199af06e009d1d4d6a0f8894
ipmsteven <steven.lyl147@gmail.com>
2019-02-04 17:48:05 -0800

    add ssh uri support for github

--- src/WebLinks/GitHubWebProvider.cs
+++ src/WebLinks/GitHubWebProvider.cs
@@ -1,27 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Mjcheetham.WebLinks
 {
     internal class GitHubWebProvider : IWebProvider
     {
         public string Name => "GitHub";
+        private string SshUriPattern => @"(.+)@(?'host'.+):(?'organization'[^\/]+)\/(?'repo'[^\/]+)\/?";
 
         public bool CanHandle(string repositoryUrl)
         {
-            try
-            {
-                var uri = new Uri(repositoryUrl);
-                return uri.Host.Equals("github.com");
-            }
-            catch (UriFormatException)
-            {
-                return false;
-            }
+            return IsSshUri(repositoryUrl) || IsHttpUri(repositoryUrl);
         }
 
         public string CreateFileUrl(string repositoryUrl, string relativePath, VersionInformation version, SelectionInformation selection)
         {
-            var sb = new UriBuilder(repositoryUrl);
+            var baseUrl = ExtractBaseFileUrl(repositoryUrl);
+            var sb = new UriBuilder(baseUrl);
 
             string versionString;
             if (version?.BranchName != null)
@@ -57,6 +52,50 @@ namespace Mjcheetham.WebLinks
             }
 
             return sb.ToString();
+        }
+
+        private bool IsSshUri(string repositoryUrl)
+        {
+            Match match = Regex.Match(repositoryUrl, SshUriPattern);
+
+            if (match.Success)
+            {
+                string hostName = match.Groups["host"].Value;
+                return StringComparer.OrdinalIgnoreCase.Equals(hostName, "github.com");
+            }
+            return false;
+        }
+
+        private bool IsHttpUri(string repositoryUrl)
+        {
+            try
+            {
+                var uri = new Uri(repositoryUrl);
+                return uri.Host.Equals("github.com");
+            }
+            catch (UriFormatException)
+            {
+                return false;
+            }
+        }
+        private string ExtractBaseFileUrl(string repositoryUrl)
+        {
+            if (IsSshUri(repositoryUrl))
+            {
+                Match match = Regex.Match(repositoryUrl, SshUriPattern);
+                string organization = match.Groups["organization"].Value;
+                string repo = match.Groups["repo"].Value;
+
+                return $"https://github.com/{organization}/{repo}";
+            }
+            else
+            {
+                if (repositoryUrl.EndsWith(".git", StringComparison.OrdinalIgnoreCase))
+                {
+                    return repositoryUrl.Substring(0, repositoryUrl.Length - ".git".Length);
+                }
+                return repositoryUrl;
+            }
         }
     }
 }
